ed09ed6b398b50fa9906db5f2f51e9573bfd99be
Thiagocord <thiago_cordeiro_84@hotmail.com>
2026-03-22 12:00:48 -0300

    up

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// 1 - pegar informações com input do form teremos: Data, local (destino), hora saida, hora chegada
+// 2 - mostrar a soma do tempo da duração da viagem (hora saida e chegada)
+// 3 - mostrar a soma de destino do dia, da semana, do mês...
+
+window.addEventListener("load", () => {
+    document.getElementById("inData").valueAsDate = new Date();
+});
+
+
